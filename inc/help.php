6315c5b8dd36228c80e546974e2df62b5ed24528
nQQb <51533421+nQQb@users.noreply.github.com>
2019-06-12 11:48:46 +0200

    Hilfe fertig

--- inc/help.php
+++ inc/help.php
@@ -2,7 +2,7 @@
     <h3>Kurze Beschreibung zu unsere Website</h3>
     <p>
 
-      Auf dieser Website können User Fotos hochladen, taggen und mit anderen Usern austauschen. </br>
+      Auf dieser Website können User Fotos hochladen, taggen und mit anderen Usern austauschen, etc. <br>
       Diese Seite enthält eine Navbar mit:
       <ul>
         <li>Home, wo sich die Hauptseite mit den Fotos von den Gündern befindet.</li>
@@ -11,13 +11,21 @@
         <li>Eine Registrierung, dammit man die seiten benutzen kann.</li>
         <li>Ein login, wo sich die registrierten Nutzer einlogen.</li>
         <li>Eine Galerie, wo die User Fotos hochladen und verwalten können.</li>
+        <li>Eine Profilverwaltung wo man das Passwort ändern kann</li>
+        <li>Ein Loutout button mit den man sich wieder abmelden kann</li>
       </ul>
     </p>
     <p>
-      Damit  man Fotos hochladet und verwalten kann, müss man sich zuerst registrieren.
-      Nachdem man registriert ist, tut man sich einlogen, un schon hat man die Möglichkeit Fotos und zu verwalten.
-    </p>
-    <p>
-      Diese Seite enthält
+      Damit  man Fotos hochladet und verwalten kann, müss man sich zuerst registrieren: <br>
+      <img src="pictures\anleitung\reg.PNG" alt="reg" height = "300" ><br>
+      Nachdem man registriert ist, tut man sich einlogen. Hier hat man wiederung din Möglichkeit einzustellen ob der User
+      Passwort und ID gespeichert werden soll oder nicht: <br>
+      <<img src="pictures\anleitung\log.PNG" alt="log" height = "200"><br>
+      Und schon hat man die Möglichkeit Fotos hochtzuladen und zu verwalten:<br>
+      <img src="pictures\anleitung\bildver.PNG" alt="gal" width = "600"><br>
+      Profilverwaltung:<br>
+      <img src="pictures\anleitung\profil.PNG" alt="profil" height = "400"><br>
+      Und wenn man keine lust mehr hat, kann man sich einfach auslogen:<br>
+      <img src="pictures\anleitung\out.PNG" alt="out">
     </p>
 </div>
